290782e244277560e57e3febc425188d4c6f5f65
wiscaksono <wwicaksono96@gmail.com>
2022-04-13 19:31:39 +0700

    add gallery pages

--- pages/praktikum/siskon/tugas-rumah.jsx
+++ pages/praktikum/siskon/tugas-rumah.jsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 export default function TugasRumah() {
   return (
-    <iframe
-      className="w-full h-[800px]"
-      src="https://drive.google.com/file/d/1IVShmvuaZcxzRRwMEjcjOvwdIEOpQxDB/preview"
-      allow="autoplay"
-    ></iframe>
+    <div>
+      <Link
+        href="https://drive.google.com/u/0/uc?id=1IVShmvuaZcxzRRwMEjcjOvwdIEOpQxDB&export=download"
+        passHref
+      >
+        <button className="text1 text-white capitalize py-[12px] px-[32px] block bg-red rounded-[5px] border border-none mb-[37px]">
+          Download
+        </button>
+      </Link>
+
+      <iframe
+        className="w-full h-[800px]"
+        src="https://drive.google.com/file/d/1IVShmvuaZcxzRRwMEjcjOvwdIEOpQxDB/preview"
+        allow="autoplay"
+      ></iframe>
+    </div>
   );
 }
